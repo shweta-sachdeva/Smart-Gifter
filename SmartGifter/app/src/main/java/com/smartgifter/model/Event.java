6b856b06dfd836d498881473538ba7e9f17fdaf5
shweta-sachdeva <ss8740@nyu.edu>
2016-04-07 19:16:54 -0400

    Clean up of previous code and adding models -User and Events

---
+++ SmartGifter/app/src/main/java/com/smartgifter/model/Event.java
@@ -0,0 +1,150 @@
+package com.smartgifter.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Calendar;
+
+/**
+ * Created by Shweta Sachdeva on 4/7/2016.
+ */
+public class Event implements Parcelable {
+
+    private int eventId;
+    private String eventTitle;
+    private Calendar eventDate;
+    private int eventOwnerId;
+    private String itemName;
+    private String itemDescription;
+    private int itemPrice;
+
+    /**
+     * Parcelable creator. Do not modify this function.
+     */
+    public static final Parcelable.Creator<Event> CREATOR = new Parcelable.Creator<Event>() {
+        public Event createFromParcel(Parcel p) {
+            return new Event(p);
+        }
+        public Event[] newArray(int size) {
+            return new Event[size];
+        }
+    };
+
+    //Default constructor
+    public Event() {
+    }
+
+    /**
+     * Create a Event model object from a Parcel. This
+     * function is called via the Parcelable creator.
+     */
+    public Event(Parcel p) {
+        eventId = p.readInt();
+        eventTitle = p.readString();
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(p.readLong());
+        eventDate = cal;
+        eventOwnerId = p.readInt();
+        itemName = p.readString();
+        itemDescription = p.readString();
+        itemPrice = p.readInt();
+    }
+
+    /**
+     * Create a Event model object from arguments
+     */
+    public Event(int eventId,String eventTitle,Calendar eventDate,int eventOwnerId,String itemName, String itemDescription,int itemPrice) {
+        this.eventId = eventId;
+        this.eventTitle = eventTitle;
+        this.eventDate = eventDate;
+        this.eventOwnerId = eventOwnerId;
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemPrice = itemPrice;
+    }
+
+
+    /**
+     * Serialize Event object by using writeToParcel.
+     * This function is automatically called by the
+     * system when the object is serialized.
+     *
+     * @param dest Parcel object that gets written on
+     * serialization. Use functions to write out the
+     * object stored via your member variables.
+     *
+     * @param flags Additional flags about how the object
+     * should be written. May be 0 or PARCELABLE_WRITE_RETURN_VALUE.
+     * In our case, you should be just passing 0.
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.eventId);
+        dest.writeString(this.eventTitle);
+        dest.writeLong(this.eventDate.getTimeInMillis());
+        dest.writeInt(this.eventOwnerId);
+        dest.writeString(this.itemName);
+        dest.writeString(this.itemDescription);
+        dest.writeInt(this.itemPrice);
+    }
+
+    public String getEventTitle() {
+        return eventTitle;
+    }
+
+    public void setEventTitle(String eventTitle) {
+        this.eventTitle = eventTitle;
+    }
+
+    public int getEventIdId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public Calendar getEventDate() {
+        return eventDate;
+    }
+    public void setEventDate(Calendar eventDate) {
+        this.eventDate = eventDate;
+    }
+    public int getEventOwnerId() {
+         return eventOwnerId;
+    }
+    public void setEventOwnerId() {
+        this.eventOwnerId = eventOwnerId;
+    }
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    /**
+     * Do not implement
+     */
+    @Override
+    public int describeContents() {
+        // Do not implement!
+        return 0;
+    }
+}
---
+++ SmartGifter/app/src/main/java/com/smartgifter/model/User.java
@@ -0,0 +1,95 @@
+package com.smartgifter.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Shweta Sachdeva on 4/7/2016.
+ */
+public class User implements Parcelable {
+
+    private int fbUserId;
+    private String fbUserName;
+
+    /**
+     * Parcelable creator. Do not modify this function.
+     */
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        public User createFromParcel(Parcel p) {
+            return new User(p);
+        }
+
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    //Default constructor
+    public User() {
+    }
+
+    /**
+     * Create a User model object from a Parcel. This
+     * function is called via the Parcelable creator.
+     *
+     * @param p The Parcel used to populate the
+     * Model fields.
+     */
+    public User(Parcel p) {
+        fbUserId = p.readInt();
+        fbUserName = p.readString();
+    }
+
+    /**
+     * Create a User model object from arguments
+     */
+    public User(int fbUserId,String fbUserName) {
+        this.fbUserId = fbUserId;
+        this.fbUserName = fbUserName;
+    }
+
+
+    /**
+     * Serialize User object by using writeToParcel.
+     * This function is automatically called by the
+     * system when the object is serialized.
+     *
+     * @param dest Parcel object that gets written on
+     * serialization. Use functions to write out the
+     * object stored via your member variables.
+     *
+     * @param flags Additional flags about how the object
+     * should be written. May be 0 or PARCELABLE_WRITE_RETURN_VALUE.
+     * In our case, you should be just passing 0.
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.fbUserId);
+        dest.writeString(this.fbUserName);
+    }
+
+    public String getName() {
+        return fbUserName;
+    }
+
+    public void setName(String fbUserName) {
+        this.fbUserName = fbUserName;
+    }
+
+    public int getId() {
+        return fbUserId;
+    }
+
+    public void setId(int fbUserId) {
+        this.fbUserId = fbUserId;
+    }
+
+    /**
+     * Do not implement
+     */
+    @Override
+    public int describeContents() {
+        // Do not implement!
+        return 0;
+    }
+}
